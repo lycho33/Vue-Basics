1ecfcef0a5cb9d9d51c4d2bc4c944754fd49d577
lycho33 <cho.lyds@gmail.com>
2022-08-25 16:09:17 -0400

    added a counter and played around with form

--- app.js
+++ app.js
@@ -7,16 +7,26 @@ const app = Vue.createApp({
             name: 'John',
             url: 'https://i.pinimg.com/originals/f4/cc/23/f4cc2387d351b39d11fc106ac3aea34a.jpg',
             counter: 0,
+            confirmedName: '',
         }
     },
     methods: {
-        ageCalc() {
-            return this.age + 5
-        },
+        ageCalc() { return this.age + 5 },
         randomNum() {
             const random = Math.random();
             console.log(random)
             return random;
+        },
+        add(num) { this.counter += num; },
+        minus(num) { this.counter -= num; },
+        setName(e, lastName) { 
+            this.name = e.target.value + ' ' + lastName;
+        },
+        submitForm(e) {
+            alert('Submitted!')
+        },
+        confirmInput() {
+            this.confirmedName = this.name;
         }
     }
 });
