f27ce36d45317db1cd751e1f07076222104e66d6
lycho33 <cho.lyds@gmail.com>
2022-08-25 14:19:13 -0400

    added counter buttons with JS

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const app = Vue.createApp({
             //like a prop, can be INTERPOLATED in HTML
             age: 35,
             name: 'John',
+            url: 'https://i.pinimg.com/originals/f4/cc/23/f4cc2387d351b39d11fc106ac3aea34a.jpg',
+            counter: 0,
         }
     },
     methods: {
         ageCalc() {
             return this.age + 5
+        },
+        randomNum() {
+            const random = Math.random();
+            console.log(random)
+            return random;
         }
     }
 });
