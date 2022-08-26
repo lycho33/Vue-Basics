a448c679e9bb5e95b069fc642adaafb774ebe139
lycho33 <cho.lyds@gmail.com>
2022-08-25 23:09:42 -0400

    include some computed/watch methods for counter and name input

--- app.js
+++ app.js
@@ -5,12 +5,45 @@ const app = Vue.createApp({
             //like a prop, can be INTERPOLATED in HTML
             age: 35,
             name: 'John',
+            lastName: '',
+            // fullName: '',
             url: 'https://i.pinimg.com/originals/f4/cc/23/f4cc2387d351b39d11fc106ac3aea34a.jpg',
             counter: 0,
             confirmedName: '',
             message: '',
             confirmedMessage: '',
         }
+    },
+    watch: {
+        counter(value) {
+            if(value > 50) {
+                this.counter = 0;
+            }
+        }
+        // name(value) {
+        //     if(value === ''){
+        //         this.fullName = '';
+        //     } else {
+        //         this.fulName = value + ' ' + this.lastName; 
+        //     }
+        // },
+        // lastName(value) {
+        //     if(value === ''){
+        //         this.fullName = '';
+        //     } else {
+        //         this.fulName = this.name + ' ' + value; 
+        //     }
+        // }
+    },
+    computed: {
+        fullname() {
+            if(this.name === '' || this.lastName === ''){
+                return '';
+            } else {
+                return this.name + ' ' + this.lastName;
+            }
+        }
+
     },
     methods: {
         ageCalc() { return this.age + 5 },
@@ -21,8 +54,15 @@ const app = Vue.createApp({
         },
         add(num) { this.counter += num; },
         minus(num) { this.counter -= num; },
+        outputFullname() {
+            if(this.name === ''){
+                return '';
+            } else {
+                return this.name + ' ' + 'Robinson'
+            }
+        },
         setName(e, lastName) { 
-            this.name = e.target.value + ' ' + lastName;
+            this.name = e.target.value;
         },
         submitForm(e) {
             alert('Submitted!')
@@ -33,6 +73,9 @@ const app = Vue.createApp({
         },
         setMessage(e) {
             this.message = e.target.value;
+        },
+        resetInput() {
+            this.name = '';
         }
     }
 });
