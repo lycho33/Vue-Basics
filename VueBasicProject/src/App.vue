d96c568574f6e59ac969f7ab368fc0cd01e98a57
lycho33 <cho.lyds@gmail.com>
2022-08-30 10:22:42 -0400

    add new component to render 1 friend data and import to App

--- VueBasicProject/src/App.vue
+++ VueBasicProject/src/App.vue
@@ -1,90 +1,33 @@
 <template>
-  <h1>Hello</h1>
+    <section>
+        <h1>My Friends</h1>
+        <ul>
+            <friend-contact/>
+        </ul>
+    </section>
 </template>
 
 <script>
-  const app = {
+import FriendContact from './components/FriendContact.vue'
+export default {
+  components: { FriendContact },
     data() {
-      return {
-        friends: [
-          {
-            id: 'manuel',
-            name: 'Manuel Lorenz',
-            phone: '0123 45678 90',
-            email: 'manuel@localhost.com'
-          },
-            {
-            id: 'Julie',
-            name: 'Julie Robinson',
-            phone: '0123 45678 90',
-            email: 'julie@localhost.com'
-          }
-        ],
-      }
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '0123 45678 90',
+                    email: 'manuel@localhost.com'
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '0243 34687 12',
+                    email: 'julie@localhost.com'
+                }
+            ]
+        }
     }
-  }
+}
 </script>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- VueBasicProject/src/main.js
+++ VueBasicProject/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import FriendContact from './components/FriendContact.vue'
 // import router from './router'
 
 import './assets/main.css'
@@ -7,5 +8,7 @@ import './assets/main.css'
 const app = createApp(App)
 
 // app.use(router)
+
+app.component('friend-contact', FriendContact)
 
 app.mount('#app')
