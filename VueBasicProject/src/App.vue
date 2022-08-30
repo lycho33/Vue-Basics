68a214b6c4e45c901274f14abf545f712df1b37c
lycho33 <cho.lyds@gmail.com>
2022-08-30 10:45:03 -0400

    passed friend data as props

--- VueBasicProject/src/App.vue
+++ VueBasicProject/src/App.vue
@@ -2,7 +2,16 @@
     <section>
         <header><h1>My Friends</h1></header>
         <ul>
-            <friend-contact/>
+            <friend-contact 
+                name="Maunel Lorenz" 
+                phone-number='0123 45678 90' 
+                email-address='manuel@localhost.com' 
+            />
+            <friend-contact 
+                name="julie" 
+                phone-number='0243 34687 12' 
+                email-address='julie@localhost.com' 
+            />
         </ul>
     </section>
 </template>
