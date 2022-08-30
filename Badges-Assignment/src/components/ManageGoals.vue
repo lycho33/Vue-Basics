ad5bb6c357e98bd0749d52212863ff3147d5ca1e
lycho33 <cho.lyds@gmail.com>
2022-08-30 19:48:07 -0400

    new component

---
+++ Badges-Assignment/src/components/ManageGoals.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Manage Goals</h2>
+        <input type="text" ref="goal"/>
+        <button @click="setGoal">Set Goal</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        setGoal() {
+            const enteredValue = this.$refs.goal.value;
+            if(enteredValue === ''){
+                
+            }
+        }
+    }
+}
+</script>
