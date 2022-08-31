34e1ffb71b8fc8f40f2f38a18b64f1e8415735fe
lycho33 <cho.lyds@gmail.com>
2022-08-30 23:42:04 -0400

    moved HTML to new component

---
+++ ResourceApp/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul>
+        <learning-resource 
+            v-for="resource in resources" 
+            :key="resource.id"
+            :title="resource.title"
+            :description="resource.description"
+            :link="resource.link"
+        >
+        </learning-resource>
+    </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue'
+
+export default {
+    components: {
+        LearningResource,
+    },
+    props: ['resources'],
+}
+</script>
