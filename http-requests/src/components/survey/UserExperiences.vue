69462c07c9edf7745f687089bb9e4cf8d44445ae
lycho33 <cho.lyds@gmail.com>
2022-08-31 17:14:12 -0400

    featch error handling

--- http-requests/src/components/survey/UserExperiences.vue
+++ http-requests/src/components/survey/UserExperiences.vue
@@ -6,8 +6,11 @@
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">
+        {{ error }}
+      </p>
       <p v-else-if="!isLoading && (!results || results.length === 0) ">No stored experiences found. Start adding some survey results first.</p>
-      <ul v-else-if="!isLoading && results.length > 0">
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -31,11 +34,13 @@ export default {
     return {
         results: [],
         isLoading: false,
+        error: null,
     }
   },
   methods: {
     loadExperiences() {
         this.isLoading = true;
+        this.error = null;
         fetch('https://vue-http-demo-58861-default-rtdb.firebaseio.com/surveys.json')
             .then((res) => {
                 if(res.ok) {
@@ -49,6 +54,11 @@ export default {
                     res.push({id: id, name: data[id].name, rating: data[id].rating});
                 }
                 this.results = res;
+            })
+            .catch((error) => {
+                console.log(error)
+                this.isLoading = false;
+                this.error = 'Failed to fetch data - please try again'
             })
     }
   },
