6572bf8abd979da1f8a48f55740f382b28cadb5d
lycho33 <cho.lyds@gmail.com>
2022-08-30 23:22:05 -0400

    new project with dummy data

---
+++ ResourceApp/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <ul>
+        <li v-for="resource in storedResources">
+            {{ resource.title }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                { 
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                { 
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to google...',
+                    link: 'https://google.org'
+                }
+            ],
+        }
+    }
+}
+</script>
