48c57ed71270f811f9731c421992fcc974466293
lycho33 <cho.lyds@gmail.com>
2022-08-31 12:03:55 -0400

    cache input data

--- ResourceApp/src/components/learning-resources/TheResource.vue
+++ ResourceApp/src/components/learning-resources/TheResource.vue
@@ -14,7 +14,9 @@
                 Add Resource
         </base-button>
     </base-card>
-    <component :is="selectedTab"></component>
+    <keep-alive>
+        <component :is="selectedTab"></component>
+    </keep-alive>
     </div>
 </template>
 
