ede2acbc9a9bdc246bd3f52ce5b774c5185a39b4
lycho33 <cho.lyds@gmail.com>
2022-08-31 10:26:04 -0400

    styled buttons + include dynamic components

---
+++ ResourceApp/src/components/learning-resources/TheResource.vue
@@ -0,0 +1,65 @@
+<template>
+    <base-card>
+        <base-button 
+            @click="setSelectedTab('stored-resources')" :mode="StoredResButtonMode"
+            >
+                Store Resources
+            </base-button>
+        <base-button 
+            @click="setSelectedTab('add-resouce')"
+            :mode="AddResButtonMode"
+        >
+            Add Resource
+        </base-button>
+    </base-card>
+    <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from '../learning-resources/StoredResources.vue'
+import AddResource from './AddResource.vue'
+
+export default {
+    components: {
+        StoredResources,
+        AddResource,
+    },
+    data() {
+        return {
+            selectedTab: 'stored-resource',
+            storedResources: [
+                { 
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                { 
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to google...',
+                    link: 'https://google.org'
+                }
+            ],
+        }
+    },
+    provide() {
+        return {
+            resources: this.storedResources
+        }
+    },
+    computed: {
+        StoredResButtonMode(tab) {
+            return this.selectedTab ==='stored-resources' ? null : 'flat';
+        },
+        AddResButtonMode(tab) {
+            return this.selectedTab === 'add-resouces' ? null : 'flat';
+        }
+    },
+    methods: {
+        setSelectedTab(tab) {
+            this.selectedTab = tab;
+        }
+    }
+}
+</script>
