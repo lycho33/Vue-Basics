298e0ca347520db3c9b293a9d21b1acf80f02309
lycho33 <cho.lyds@gmail.com>
2022-08-31 20:34:39 -0400

    event handler to relocate to another page

--- routerMultiPage/src/components/users/UsersList.vue
+++ routerMultiPage/src/components/users/UsersList.vue
@@ -1,4 +1,5 @@
 <template>
+  <button @click="confirmInput">Confirm</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -12,6 +13,12 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  methods: {
+    confirmInput() {
+        // used to automatically navigate to another page once method is invoke/event occurs
+        this.$router.push('/teams');
+    }
+  }
 };
 </script>
 
