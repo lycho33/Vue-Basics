ad3231dc5f2b0a9030d91df4386a96145e156b48
lycho33 <cho.lyds@gmail.com>
2022-09-01 10:06:58 -0400

    update files

--- TransitionsAnimations/src/main.js
+++ TransitionsAnimations/src/main.js
@@ -1,6 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-import './assets/main.css'
+import App from './App.vue';
+import BaseModal from './components/BaseModal.vue';
+import AllUsers from './pages/AllUsers.vue';
+import CourseGoals from './pages/CourseGoals.vue';
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: AllUsers },
+    { path: '/goals', component: CourseGoals }
+  ]
+});
+
+const app = createApp(App);
+
+app.component('base-modal', BaseModal);
+
+app.use(router);
+
+router.isReady().then(function() {
+  app.mount('#app');
+});
+
+
