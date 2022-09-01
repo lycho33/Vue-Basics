bb73459130497e15a21b1a5e31caededed49f278
lycho33 <cho.lyds@gmail.com>
2022-08-31 22:23:52 -0400

    routes

--- routerMultiPage/src/components/users/UsersList.vue
+++ routerMultiPage/src/components/users/UsersList.vue
@@ -1,8 +1,11 @@
 <template>
-  <button @click="confirmInput">Confirm</button>
-  <ul>
-    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
-  </ul>
+    <div>
+        <button @click="confirmInput">Confirm</button>
+        <button @click="saveChanges">Save Changes</button>
+        <ul>
+            <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+        </ul> 
+  </div>
 </template>
 
 <script>
@@ -13,12 +16,36 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+        changesSave: false,
+    }
+  },
   methods: {
     confirmInput() {
         // used to automatically navigate to another page once method is invoke/event occurs
         this.$router.push('/teams');
+    },
+    saveChanges() {
+        this.changesSave = true;
     }
-  }
+  },
+  //confirm this navigation action - Naviation Guard
+  beforeRouteEnter(to, from, next) {
+    next();
+  },
+  //help make sure that users don't accidentally forget their input
+    beforeRouteLeave(to, from, next){
+        //console.log(to, from)
+        if(this.changesSave){
+            //confirms the navigation
+            next()
+        } else {
+            const userWantsToLeave = prompt('Are you sure? You go unsave changes!');
+            //cancels the naviagtion 
+            next(userWantsToLeave);
+        }
+    }
 };
 </script>
 
