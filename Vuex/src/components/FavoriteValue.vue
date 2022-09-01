e63fa5d87bde58a24345c7178d4c839905844da8
lycho33 <cho.lyds@gmail.com>
2022-09-01 17:30:32 -0400

    update store

---
+++ Vuex/src/components/FavoriteValue.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3>{{ counter }}</h3>
+  <p>We do more...</p>
+</template>
+
+<script>
+export default {
+  computed: {
+    counter() {
+      //there [] because of namespace in index.js
+      return this.$store.getters['numbers/normalizedCounter'];
+    },
+  },
+};
+</script>
