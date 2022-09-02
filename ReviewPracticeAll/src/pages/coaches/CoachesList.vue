30b57e9e8f5109084d3bcc8217b9cf471a17f152
lycho33 <cho.lyds@gmail.com>
2022-09-02 17:50:00 -0400

    styled coaches details with slot + global components

--- ReviewPracticeAll/src/pages/coaches/CoachesList.vue
+++ ReviewPracticeAll/src/pages/coaches/CoachesList.vue
@@ -4,22 +4,36 @@
             FILTER
         </section>
         <section>
-            <div class="controls">
-                <button>Refresh</button>
-                <router-link to="/register">Register as Coach</router-link>
-            </div>
-            <ul v-if="hasCoaches">
-                <li v-for="coach in filteredCoaches" :key="coach.id">
-                    {{ coach.firstName }}
-                </li>
-            </ul>
-            <h3 v-else>No Coaches Found</h3>
+            <base-card>
+                <div class="controls">
+                    <base-button mode="outline">Refresh</base-button>
+                    <!-- sends link as a prop and is automatically TRUE -->
+                    <router-link link to="/register">Register as Coach</router-link>
+                </div>
+                <ul v-if="hasCoaches">
+                    <li v-for="coach in filteredCoaches" :key="coach.id">
+                        <CoachItem 
+                            :id="coach.id"
+                            :first-name="coach.firstName"
+                            :last-name="coach.lastName"
+                            :rate="coach.hourlyRate"
+                            :areas="coach.areas"    
+                        />
+                    </li>
+                </ul>
+                <h3 v-else>No Coaches Found</h3>
+            </base-card>
         </section>
     </div>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue'
+
 export default {
+    components: {
+        CoachItem,
+    },
     computed: {
         filteredCoaches() {
             return this.$store.getters['coaches/coaches'];
@@ -32,5 +46,14 @@ export default {
 </script>
 
 <style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
