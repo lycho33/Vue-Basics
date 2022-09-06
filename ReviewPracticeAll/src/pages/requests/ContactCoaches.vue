c746b9507aa8c9b65c28ac4f8e0b6ce3e940ecf1
lycho33 <cho.lyds@gmail.com>
2022-09-05 20:40:17 -0400

    form info + styles

--- ReviewPracticeAll/src/pages/requests/ContactCoaches.vue
+++ ReviewPracticeAll/src/pages/requests/ContactCoaches.vue
@@ -1,15 +1,70 @@
 <template>
     <div>
-        Contacting Coaches
+        <form>
+            <div class="form-control">
+                <label for="email">YOur E-mail</label>
+                <input type="email" id="email" />
+            </div>
+            <div class="form-control">
+                <label for="message">Message</label>
+                <textarea rows="5" id="message"></textarea>
+            </div>
+            <div class="actions">
+                <base-button>Send Message</base-button>
+
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
+import BaseButton from '../../../../ResourceApp/src/components/UI/BaseButton.vue'
 export default {
+  components: { BaseButton },
     
 }
 </script>
 
 <style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
 
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
 </style>
